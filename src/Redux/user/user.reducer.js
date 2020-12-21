ffe27a2af158261ec9f04fd70bffdc49eb9df38c
Michael Lewis <wmichael.lewis@outlook.com>
2020-12-20 21:22:39 -0800

    heading into Redux review

---
+++ src/Redux/user/user.reducer.js
@@ -0,0 +1,11 @@
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
+const userReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case '':
+            
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
 
 
 ReactDOM.render(
   <React.StrictMode>
+  <Provider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>,
   </React.StrictMode>,
   document.getElementById("root")
 );
