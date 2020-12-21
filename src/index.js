20fa9ce7716cc26e791d8049f5c9f500efae6f59
Michael Lewis <wmichael.lewis@outlook.com>
2020-12-20 21:38:47 -0800

    base Redux setup for now

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 
+import store from './Redux/store';
+
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
